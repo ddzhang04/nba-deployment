002332353f85f0c861c4056257b05585f88bcf33
ddzhang04 <dzhang0916@gmail.com>
2025-06-08 20:23:58 -0400

    chabge

--- src/App.jsx
+++ src/App.jsx
@@ -757,8 +757,6 @@ const NBAGuessGame = () => {
           </div>
         </div>
       </div>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7430051034208796"
-     crossorigin="anonymous"></script>
     </div>
   );
 };
