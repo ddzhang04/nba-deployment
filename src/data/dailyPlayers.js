2a8163d765defbf3c787119cadd123e22cdc7dd0
Joshua Moy <jmoy2077@gmail.com>
2026-03-15 04:10:28 -0400

    changes

--- src/data/dailyPlayers.js
+++ src/data/dailyPlayers.js
@@ -16,7 +16,7 @@ export const DAILY_PUZZLE_EPOCH = '2026-03-15';
 
 export const DAILY_PLAYERS = [
   'Bam Adebayo',   // Day 0 = Daily #1
-  'Jabari Smith Jr.',        // Day 1 = Daily #2
+  'Kevin Durant',        // Day 1 = Daily #2
   'James Harden',       // Day 2 = Daily #3
   'Jayson Tatum',       // Day 3 = Daily #4
   'Dwyane Wade',        // Day 4 = Daily #5
@@ -30,6 +30,10 @@ export const DAILY_PLAYERS = [
   'Kevin Love',         // Day 12 = Daily #13
   'Kemba Walker',       // Day 13 = Daily #14
   'Hassan Whiteside',   // Day 14 = Daily #15
+
+// WHEN I LAUNCH THE APP ILL RESTART AT THE TOP OF THE LIST
+// 3/31/26 ? Also need current season data
+
   // Add more names here for future days. Example:
   // 'LeBron James',
   // 'Anthony Davis',
