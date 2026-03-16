3d9cbb474cf732bffbe789d8a0df7ae14b3e42c5
Joshua Moy <jmoy2077@gmail.com>
2026-03-16 14:51:26 -0400

    epoch

--- src/data/dailyPlayers.js
+++ src/data/dailyPlayers.js
@@ -12,24 +12,24 @@
  * To change the "start" of the calendar: change DAILY_PUZZLE_EPOCH to the date you want to be Day 0 (Daily #1).
  */
 
-export const DAILY_PUZZLE_EPOCH = '2026-03-15';
+export const DAILY_PUZZLE_EPOCH = '2026-03-14';
 
 export const DAILY_PLAYERS = [
   'Bam Adebayo',   // Day 0 = Daily #1
   'Kevin Durant',        // Day 1 = Daily #2
-  'James Harden',       // Day 2 = Daily #3
+  'LeBron James',       // Day 2 = Daily #3
   'Jayson Tatum',       // Day 3 = Daily #4
   'Dwyane Wade',        // Day 4 = Daily #5
   'Kevin Garnett',      // Day 5 = Daily #6
-  'Paul Pierce',        // Day 6 = Daily #7
+  'James Harden',        // Day 6 = Daily #7
   'Tim Duncan',         // Day 7 = Daily #8
   'Cade Cunningham',    // Day 8 = Daily #9
   'Stephen Curry',      // Day 9 = Daily #10
-  'Klay Thompson',      // Day 10 = Daily #11
+  'Kobe Bryant',      // Day 10 = Daily #11
   'Kyrie Irving',       // Day 11 = Daily #12
   'Kevin Love',         // Day 12 = Daily #13
   'Kemba Walker',       // Day 13 = Daily #14
-  'Hassan Whiteside',   // Day 14 = Daily #15
+  'Russell Westbrook',   // Day 14 = Daily #15
 
 // WHEN I LAUNCH THE APP ILL RESTART AT THE TOP OF THE LIST
 // 3/31/26 perhaps
